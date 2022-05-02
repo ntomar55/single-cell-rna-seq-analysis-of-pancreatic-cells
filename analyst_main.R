427cc50b023c33bbe65dfca6f1a43f6e70d9368b
DavidLenci <98466096+DavidLenci@users.noreply.github.com>
2022-05-01 22:37:05 -0400

    Update analyst_main.R
    added comments

--- analyst_main.R
+++ analyst_main.R
@@ -4,7 +4,7 @@ library("tidyverse")
 library("Seurat")
 library("patchwork")
 library("DESeq2")
-
+#read in data
 read_data <- function(path){
   
   data <- readRDS(file=path)
@@ -16,6 +16,7 @@ data <- read_data("/usr4/bf527/dlenci/Documents/BF528-proj4-nogit/data/GSM223076
 
 data
 
+# identify markers using differrential expression
 get_markers <- function(seurat_data, markers) {
   
   data_markers <- FindAllMarkers(seurat_data, min.pct = 0.2) %>%
@@ -26,23 +27,25 @@ get_markers <- function(seurat_data, markers) {
   return(data_markers)
   
 }
+#markers to use for heatmap
 markers <- c("SST", "INS", "COL1A1", "COL1A2", "FN1", "REG1B", "SPINK1", "CTRB2",
              "GCG", "KRT19", "ACER3", "RPS6KA5", "GC", "KRT19", "MMP7", "KRT7",
              "FCER1G", "IFI30", "TTR", "CHGB", "CRYBA2", "COL6A3", "ACP5")
 data_markers <- get_markers(data, markers)
 
+#cluster labels
 new.cluster.ids <- c("Delta", "Beta", "Stellate", "Acinar", "Alpha",
                      "Ductal", "Beta", "Oligodendrocyte", "Hepatocyte", 
                      "Stellate", "Ductal", "Acinar", "Macrophage")
 names(new.cluster.ids) <- levels(data)
 data <- RenameIdents(data, new.cluster.ids)
 
-
+#generate umap
 data_t <- RunUMAP(data, dims = 1:9)
 png("umap.png")
 DimPlot(data_t, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
 dev.off()
-
+#generate heatmap
 png("hm.png")
 DoHeatmap(data, features = data_markers$gene, label = F)
 dev.off()
